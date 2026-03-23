5451adf8b0e1b8cf2b3e6a0f6a35a6ef954b9a7d
Ana Julia <arruda.ana.mont@gmail.com>
2026-03-23 08:35:10 -0400

    5th commit

--- problemas18_03/03_1803.py
+++ problemas18_03/03_1803.py
@@ -1,22 +1,21 @@
-#CORRGIR 
 a, b, c = map(int, input().split())
 
-if a > b and a > c:
-    if b > c:
+if a >= b and a >= c:
+    if b >= c:
         print(f"{c}\n{b}\n{a}")
     else:
         print(f"{b}\n{c}\n{a}")
     
-elif b > a and b > c:
-    if a > c:
+elif b >= a and b >= c:
+    if a >= c:
         print(f"{c}\n{a}\n{b}")
     else:
         print(f"{a}\n{c}\n{b}")
     
-elif c > a and c > b:
-    if a > b:
+elif c >= a and c >= b:
+    if a >= b:
         print(f"{b}\n{a}\n{c}")        
     else:
         print(f"{a}\n{b}\n{c}")
        
-#print(f"\n{a}\n{b}\n{c}")
+print(f"\n{a}\n{b}\n{c}")
