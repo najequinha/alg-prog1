899c342838318fe125efb00ef6358fbaf4b4375c
Ana Julia <arruda.ana.mont@gmail.com>
2026-03-23 08:58:02 -0400

    6th commit

--- problemas18_03/06_1803.py
+++ problemas18_03/06_1803.py
@@ -5,83 +5,112 @@ if a > b and a > c:
         #ordem: a, b, c
         a, b, c = a, b, c
         if a >= (b + c):
-            print("NAO FROMA TRIANGULO")
-        elif (a ** 2) == (b**2+c**2):
-            print("TRIANGULO RETANGULO")
-        elif (a**2) > (b**2+c**2):
-            print("TRIANGULO OBTUSANGULO")
-        elif (a**2) < (b**2+c**2):
-            print("TRIANGULO ACUTANGULO")
-        elif a == b and b == c:
-            print("TRIANGULO EQUILATERO")
-        elif a == b and b != c and c != a or a != b and b == c and a != c or a == c and b != c and a != b:
-            print("TRIANGULO ISOSCELES")
+            print("NAO FORMA TRIANGULO")
+        else:
+            if True:
+                if a ** 2 == (b**2+c**2):
+                    print("TRIANGULO RETANGULO")
+                elif a ** 2 > (b**2+c**2):
+                    print("TRIANGULO OBTUSANGULO")
+                elif a ** 2 < (b**2+c**2):
+                    print("TRIANGULO ACUTANGULO) 
+            if True:
+                if a == b and b == c:
+                    print("TRIANGULO EQUILATERO) 
+                else:
+                    print("TRIANGULO ISOSCELES")
+
     else:
         #ordem: a, c, b
         a, b, c = a, c, b
         if a >= (b + c):
-            print("NAO FROMA TRIANGULO")
-        elif (a ** 2) == (b**2+c**2):
-            print("TRIANGULO RETANGULO")
-        elif (a**2) > (b**2+c**2):
-            print("TRIANGULO OBTUSANGULO")
-        elif (a**2) < (b**2+c**2):
-            print("TRIANGULO ACUTANGULO")
-        elif a == b and b != c and c != a or a != b and b == c and a != c or a == c and b != c and a != b:
-            print("TRIANGULO ISOSCELES")
+            print("NAO FORMA TRIANGULO")
+        else:
+            if True:
+                if a ** 2 == (b**2+c**2):
+                    print("TRIANGULO RETANGULO")
+                elif a ** 2 > (b**2+c**2):
+                    print("TRIANGULO OBTUSANGULO")
+                elif a ** 2 < (b**2+c**2):
+                    print("TRIANGULO ACUTANGULO) 
+            if True:
+                if a == b and b == c:
+                    print("TRIANGULO EQUILATERO) 
+                else:
+                    print("TRIANGULO ISOSCELES")
 
 elif b > a and b > c:
     if a > c:
         #ordem: b, a, c
         a, b, c = b, a, c
         if a >= (b + c):
-            print("NAO FROMA TRIANGULO")
-        elif (a ** 2) == (b**2+c**2):
-            print("TRIANGULO RETANGULO")
-        elif (a**2) > (b**2+c**2):
-            print("TRIANGULO OBTUSANGULO")
-        elif (a**2) < (b**2+c**2):
-            print("TRIANGULO ACUTANGULO")
-        elif a == b and b != c and c != a or a != b and b == c and a != c or a == c and b != c and a != b:
-            print("TRIANGULO ISOSCELES")
+            print("NAO FORMA TRIANGULO")
+        else:
+            if True:
+                if a ** 2 == (b**2+c**2):
+                    print("TRIANGULO RETANGULO")
+                elif a ** 2 > (b**2+c**2):
+                    print("TRIANGULO OBTUSANGULO")
+                elif a ** 2 < (b**2+c**2):
+                    print("TRIANGULO ACUTANGULO) 
+            if True:
+                if a == b and b == c:
+                    print("TRIANGULO EQUILATERO) 
+                else:
+                    print("TRIANGULO ISOSCELES")
     else:
         #ordem: b, c, a
         a, b, c = b, c, a
         if a >= (b + c):
-            print("NAO FROMA TRIANGULO")
-        elif (a ** 2) == (b**2+c**2):
-            print("TRIANGULO RETANGULO")
-        elif (a**2) > (b**2+c**2):
-            print("TRIANGULO OBTUSANGULO")
-        elif (a**2) < (b**2+c**2):
-            print("TRIANGULO ACUTANGULO")
-        elif a == b and b != c and c != a or a != b and b == c and a != c or a == c and b != c and a != b:
-            print("TRIANGULO ISOSCELES")
+            print("NAO FORMA TRIANGULO")
+        else:
+            if True:
+                if a ** 2 == (b**2+c**2):
+                    print("TRIANGULO RETANGULO")
+                elif a ** 2 > (b**2+c**2):
+                    print("TRIANGULO OBTUSANGULO")
+                elif a ** 2 < (b**2+c**2):
+                    print("TRIANGULO ACUTANGULO) 
+            if True:
+                if a == b and b == c:
+                    print("TRIANGULO EQUILATERO) 
+                else:
+                    print("TRIANGULO ISOSCELES")
 
 elif c > a and c > b:
     if a > b:
         #ordem: c, a, b
         a, b, c = c, a, b
         if a >= (b + c):
-            print("NAO FROMA TRIANGULO")
-        elif (a ** 2) == (b**2+c**2):
-            print("TRIANGULO RETANGULO")
-        elif (a**2) > (b**2+c**2):
-            print("TRIANGULO OBTUSANGULO")
-        elif (a**2) < (b**2+c**2):
-            print("TRIANGULO ACUTANGULO")
-        elif a == b and b != c and c != a or a != b and b == c and a != c or a == c and b != c and a != b:
-            print("TRIANGULO ISOSCELES")
+            print("NAO FORMA TRIANGULO")
+        else:
+            if True:
+                if a ** 2 == (b**2+c**2):
+                    print("TRIANGULO RETANGULO")
+                elif a ** 2 > (b**2+c**2):
+                    print("TRIANGULO OBTUSANGULO")
+                elif a ** 2 < (b**2+c**2):
+                    print("TRIANGULO ACUTANGULO) 
+            if True:
+                if a == b and b == c:
+                    print("TRIANGULO EQUILATERO) 
+                else:
+                    print("TRIANGULO ISOSCELES")
     else:
         #ordem: c, b, a
         a, b, c = c, b, a
         if a >= (b + c):
-            print("NAO FROMA TRIANGULO")
-        elif (a ** 2) == (b**2+c**2):
-            print("TRIANGULO RETANGULO")
-        elif (a**2) > (b**2+c**2):
-            print("TRIANGULO OBTUSANGULO")
-        elif (a**2) < (b**2+c**2):
-            print("TRIANGULO ACUTANGULO")
-        elif a == b and b != c and c != a or a != b and b == c and a != c or a == c and b != c and a != b:
-            print("TRIANGULO ISOSCELES")
+            print("NAO FORMA TRIANGULO")
+        else:
+            if True:
+                if a ** 2 == (b**2+c**2):
+                    print("TRIANGULO RETANGULO")
+                elif a ** 2 > (b**2+c**2):
+                    print("TRIANGULO OBTUSANGULO")
+                elif a ** 2 < (b**2+c**2):
+                    print("TRIANGULO ACUTANGULO) 
+            if True:
+                if a == b and b == c:
+                    print("TRIANGULO EQUILATERO) 
+                else:
+                    print("TRIANGULO ISOSCELES")
