520eea85a79bbaeae89e7b30500b0fe46a814f3d
Ana Julia <arruda.ana.mont@gmail.com>
2026-03-29 19:04:48 -0400

    8th commit

---
+++ problemas23_03/04_2303.py
@@ -0,0 +1,37 @@
+p1 = input()
+p2 = input()
+p3 = input()
+
+if p1 == "vertebrado":
+    if p2 == "ave":
+        
+        if p3 == "carnivoro":
+            print("aguia")
+        
+        elif p3 == "onivoro":
+            print("pomba")
+            
+    elif p2 == "mamifero":
+        
+        if p3 == "onivoro":
+            print("homem")
+            
+        elif p3 == "herbivoro":
+            print("vaca")
+        
+        
+elif p1 == "invertebrado":
+    if p2 == "inseto":
+        
+        if p3 == "hematofogo":
+            print("pulga")
+            
+        elif p3 == "herbivoro":
+            print("lagarta")
+        
+    elif p2 == "anelideo":
+        if p3 == "hematofogo":
+            print("sanguessuga")
+            
+        elif p3 == "onivoro":
+            print("minhoca")
