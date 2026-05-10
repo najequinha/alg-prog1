ce4c96c963da3b662350858bfbfdf1b102d24d19
Ana Julia <arruda.ana.mont@gmail.com>
2026-05-10 09:10:38 -0400

    ..

--- problemas260323/01_2303.py
+++ problemas260323/01_2303.py
@@ -9,5 +9,9 @@ elif h2 < h1:
     print(f"O JOGO DUROU {horas} HORA(S)")
 
 else:
+<<<<<<< Updated upstream:problemas260323/01_2303.py
 horas = h2 - h1
+=======
+    horas = h2 - h1
+>>>>>>> Stashed changes:problemas23_03/01_2303.py
     print(f"O JOGO DUROU {horas} HORA(S)")
--- problemas260323/02_2303.py
+++ problemas260323/02_2303.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:problemas260323/02_2303.py
 #se um maior que o outro: subtrai e divide
 #se igual = 24
 
@@ -17,3 +18,33 @@ else:
     horas = tempo // 60
     minutos = tempo % 60
     print(f"O JOGO DUROU {horas} HORA(S) E {minutos} MINUTO(S)")
+=======
+h_i, m_i, h_f, m_f = map(int, input().split())
+
+if h_i == h_f and m_i == m_f:
+    print(f"O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)")
+else:
+    if h_f < h_i:
+        horas = h_f - 24 + hi
+        if m_f < m_i:
+            minutos = 60 - m_i - m_f
+            print(f"O JOGO DUROU {horas} HORA(S) E {minutos} MINUTO(S)")
+        elif m_f > m_i:
+            minutos = m_f - m_i
+            print(f"O JOGO DUROU {horas} HORA(S) E {minutos} MINUTO(S)")
+        else:
+            minutos = 0
+            print(f"O JOGO DUROU {horas} HORA(S) E {minutos} MINUTO(S)")
+        #60 - minutos + minutos
+    else:
+        horas = h_f - h_i
+        if m_f < m_i:
+            minutos = 60 - m_i - m_f
+            print(f"O JOGO DUROU {horas} HORA(S) E {minutos} MINUTO(S)")
+        elif m_f > m_i:
+            minutos = m_f - m_i
+            print(f"O JOGO DUROU {horas} HORA(S) E {minutos} MINUTO(S)")
+        else:
+            minutos = 0
+            print(f"O JOGO DUROU {horas} HORA(S) E {minutos} MINUTO(S)")
+>>>>>>> Stashed changes:problemas23_03/02_2303.py
